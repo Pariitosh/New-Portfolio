4c6b447026597e8a36edbf6acaca1cb149f81379
pariitosh <paritoshsagar25@gmail.com>
2024-01-03 19:52:08 +0530

    added anal

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { Analytics } from '@vercel/analytics/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+  <Analytics/>
     <App />
     
   
