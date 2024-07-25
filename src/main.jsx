538e8c0bb7568168c791c5c2eb35d37a03933ba4
pariitosh <paritoshsagar25@gmail.com>
2024-07-25 13:16:01 +0530

    added speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { inject } from '@vercel/analytics';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
   {inject()}
+  <SpeedInsights/>
     <App />
     
     </>
